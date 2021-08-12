00e198cdc60b4c55334177df8e03c36616ef46d0
patipan-patisampita <patipan.patisampita@gmail.com>
2021-08-12 14:47:55 +0700

    Update day1

---
+++ src/components/NavBar.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>Home | About | Contact</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "NavBar",
+  setup() {
+    return {};
+  },
+});
+</script>
+
+<style scoped></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,24 @@
 <template>
   <div class="home">
+    <NavBar />
+
+    <button class="btn btn-primary">OK</button>
+
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <!-- <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" /> -->
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
-
+// import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+// import NavBar from "../components/NavBar.vue";
+import NavBar from "@/components/NavBar.vue";
 export default defineComponent({
   name: "Home",
   components: {
-    HelloWorld,
+    // HelloWorld,
+    NavBar,
   },
 });
 </script>
